2dc3621997247e891e4d8b15c42a7b7f6ef5b226
nainssa <paulabib17@gmail.com>
2023-11-05 21:12:38 +0100

    todo done

--- web-api/src/main/java/fr/croixrouge/exposition/controller/ChatController.java
+++ web-api/src/main/java/fr/croixrouge/exposition/controller/ChatController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//todo revoir les valuers de retour
 @RestController
 @RequestMapping("/chat")
 public class ChatController extends ErrorHandler {
