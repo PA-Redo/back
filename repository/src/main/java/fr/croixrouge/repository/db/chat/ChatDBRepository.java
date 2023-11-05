8c3f5f2b831bca5e5d6ef438fc9930bf06a1411d
nainssa <paulabib17@gmail.com>
2023-11-05 21:13:30 +0100

    todo done

--- repository/src/main/java/fr/croixrouge/repository/db/chat/ChatDBRepository.java
+++ repository/src/main/java/fr/croixrouge/repository/db/chat/ChatDBRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ChatDBRepository extends CrudRepository<ChatDB, Long> {
-    //todo utiliser ça du coté beneficiaire
     @Query("select c from ChatDB c where c.beneficiaryDB.id = ?1")
     List<ChatDB> findByBeneficiaryDB_Id(Long id);
 
-    //todo utiliser ça du coté volontaire
     @Query("select c from ChatDB c")
     List<ChatDB> findAll();
 }
